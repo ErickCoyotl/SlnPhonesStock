1aef812e3493f9ac1d6196b2c5a705ac84f7d8db
ErickCoyotl <utp0141652@alumno.utpuebla.edu.mx>
2023-08-11 03:07:02 -0600

    cambio minimo

--- PhonesStock/Shared/DataContexts/SQLDBContext.cs
+++ PhonesStock/Shared/DataContexts/SQLDBContext.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace PhonesStock.Shared.DataContexts
 {
     public class SQLDBContext: DbContext
